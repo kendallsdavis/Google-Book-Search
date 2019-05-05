c575c5c3fc2481692a2e594a7e8a47dbdad77b99
Kendall Davis <kendallsdavis@gmail.com>
2019-05-05 16:27:43 -0400

    Updates

---
+++ precache-manifest.15f61ade3a3bf733c157c7f9e2cbaca6.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "83a47354cee94f3b7d41f5a2a610acd5",
+    "url": "/Google-Book-Search/index.html"
+  },
+  {
+    "revision": "01f594634190775cc27f",
+    "url": "/Google-Book-Search/static/css/main.1ac5a163.chunk.css"
+  },
+  {
+    "revision": "0d05300473b26f0b4b8f",
+    "url": "/Google-Book-Search/static/js/2.c8035158.chunk.js"
+  },
+  {
+    "revision": "01f594634190775cc27f",
+    "url": "/Google-Book-Search/static/js/main.7260f72b.chunk.js"
+  },
+  {
+    "revision": "b7607bb90801c5b9c2ea",
+    "url": "/Google-Book-Search/static/js/runtime~main.960e5d5c.js"
+  }
+]);
